72280150b7c3ff8db60c3a45971f50592cb12f8d
AshvittaS <ashvittas.cb24@bitsathy.ac.in>
2025-02-14 11:39:42 +0530

    Javascript practice

---
+++ index.js
@@ -0,0 +1,7 @@
+
+  //single line comment 
+   alert("I am studying Javascript");
+   alert(4 + 5);
+  /*Multiline command*/ 
+  console.log("Hello i doing jss");
+
---
+++ var.js
@@ -0,0 +1,6 @@
+var h="I am studying javascript";
+let n="King"
+alert(h);
+alert("the name is"+n);
+console.log(h);
+console.log(h);

4651d54dd9a75f884c176500489dfd9a26fbca91
AshvittaS <ashvittas.cb24@bitsathy.ac.in>
2025-02-14 16:02:54 +0530

    File is modified

--- var.js
+++ var.js
@@ -1,6 +1,7 @@
-var h="I am studying javascript";
-let n="King"
+var h="I am studying javascript ";
+let n="King";
+const k="Little ";
 alert(h);
-alert("the name is"+n);
+alert("the name is "+k + n);
 console.log(h);
-console.log(h);
+console.log(h + k);
